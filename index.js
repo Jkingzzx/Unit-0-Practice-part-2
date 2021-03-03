f8a272df58dd6b6f665621b92d2fba6e040403f3
Johan Argueta <jargueta229@noscihigh.org>
2021-03-03 22:12:54 +0000

    Done

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 //Unit 0 Practice Part 2
 
-/* 1.
-
-Write a function that returns the largest of
+/* 1. Write a function that returns the largest of
 two numbers.
+
+
+
 
 Example: 
 greaterThan(20, 4) => 20
 greaterThan(5, 15) => 15
 
 */
+function max(2, 1){
+  
+    if (2 > 1) {
+       console.log( 2 + " is larger than " + 1);
+    } else {
+        console.log( 2+ " is less than " + 1);
+    }
+    return;
+  
+}
 
 
 
@@ -29,6 +40,9 @@ lessThan(20, 4) => 20
 lessThan(5, 15) => 15
 
 */
+console.log(Math.min(2, 3, 1));
+// expected output: 1
+
 
 
 
@@ -49,7 +63,16 @@ arrayPrint(myArray) => 0 1 2 3 4 5
 arrayPrint(['hello', true]) => 'hello'  true
 
 */
-
+// Input array contain some elements. 
+var array = [ 'a', 'gfg', 'c', 'n' ]; 
+  
+// Here array.values() function is called. 
+var iterator = array.values(); 
+  
+// Here all the elements of the array is being printed. 
+for (let elements of iterator) { 
+  console.log(elements); 
+} 
 
 
 
@@ -66,6 +89,25 @@ counter(6) => 0 1 2 3 4 5 6
 counter(10) => 0 1 2 3 4 5 6 7 8 9 10
 
 */
+for ( var i = 1; i <= 100; i++ )
+{
+  if ( i%3 === 0 && i%5 === 0 )
+  {
+    console.log( i + " FizzBuzz" );
+  }
+  else if ( i%3 === 0 ) 
+  {
+    console.log(i+ " Fizz" );
+  }
+  else if ( i%5 === 0 ) 
+  {
+    console.log(i+ " Buzz" );
+  }
+  else
+  {
+    console.log(i);
+  }
+}
 
 
 
@@ -85,6 +127,13 @@ isEven(6) => even
 isEven(55) => odd
 
 */
+var num = 5;
+   document.write("Number = "+num+"<br>");
+   if(num % 2 == 0) {
+      document.write('Number is even!');
+   } else {
+      document.write('Number is odd!');
+   }
 
 
 
@@ -93,17 +142,3 @@ isEven(55) => odd
 
 
 
-
-/* 5.
-
-Write a function that determines if a the numbers 
-in a given array are even or odd
-
-Example: 
-
-var numArray = 
-
-isEven(6) => even
-isEven(55) => odd
-
-*/
